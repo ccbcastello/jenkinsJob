9318d4d79914fa1acf6b6d64776f1bf7e23659dd
ccbcastello <71710803+ccbcastello@users.noreply.github.com>
2022-09-23 09:05:07 -0300

    Add java files

---
+++ sample.java
@@ -0,0 +1,15 @@
+class sample {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 30;
+    int second = 30;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
