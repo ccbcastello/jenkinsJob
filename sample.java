3cdcf35e75f6d9655b3a466bc1e489456b40b01d
ccbcastello <71710803+ccbcastello@users.noreply.github.com>
2022-09-28 20:53:02 -0300

    changed first to 10

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ class sample {
   public static void main(String[] args) {
     
     System.out.println("Enter two numbers");
-    int first = 70;
+    int first = 10;
     int second = 70;
     
     System.out.println(first + " " + second);
